ba96f4d154ba23242adcf1bf664c1835975bc837
wilmer5 <wilmeruzcategui5@hotmail.com>
2018-01-06 17:03:09 -0300

    Proyecto base

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'AdminLTE PRUEBA')
+
+@section('content_header')
+    <h1>Dashboard de prueba</h1>
+@stop
+
+@section('content')
+    <p>You are logged in! dale de aqui pa abajo</p>
+@stop
